9df08d03e0d4afc4fa41719847a9e008e0839aad
unknown <nezzalinebato@gmail.com>
2024-04-19 14:27:07 +0800

    Merging, renaming and adding endpoint

--- database/migrations/2024_03_13_000902_create_violations_table.php
+++ database/migrations/2024_03_13_000902_create_violations_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->integer('card_number')->unique();
             $table->string('violation_desc');
             $table->string('violation_type');
-            $table->string('dateEnded');
-            $table->string('remarks');
+            $table->string('dateEnded')->nullable();
+            $table->string('remarks')->nullable();
             $table->timestamps();
 
         });
